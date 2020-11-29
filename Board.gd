ccc51bc6ad5d371d7a9dd035e084a6e012e66b24
Isaac Hoque <thepikachuih@gmail.com>
2020-11-28 21:07:57 -0600

    Got the turn system to work

--- Board.gd
+++ Board.gd
@@ -4,7 +4,7 @@ extends Node2D
 # VARIABLES
 var x_locations = [] # Fill with Vector2s of coordinates
 var o_locations = [] # Fill with Vector2s of coordinates
-var isXTurn = true
+var isXTurn = false
 
 
 func draw_board():
@@ -20,29 +20,26 @@ func _ready():
 
 func checkIfTaken(x, o, xTurn):
 	if xTurn: # If it is X's turn
-		for oloc in o: # Cycle through all of o's pieces
-			if oloc in x: # And if one piece matches the square the player clicked
-				return false # Don't place it
-	elif not xTurn: # If it is not X's turn
-		for xloc in x: # Cycle through all of X's pieces
-			if xloc in o: # And if one piece matches the square the player clicked
-				return false # Don't place it
+		if x in o: # If the X coordinate is in the O pieces
+			return false # Don't place it
+
+	elif not xTurn: # If it is X's turn
+		if o in x: # If the X coordinate is in the O pieces
+			return false # Don't place it
+
 	# If it is a valid square
 	return true
 
 
-func setSquare(event):
+func setSquare(event, square):
 	if isXTurn: # If it's X's turn
 		if event.button_index == BUTTON_LEFT and event.pressed: # Check if the Left Mouse Button was pressed
-			print(event.as_text(), event.pressed)
-			var square = getSquare(event.position)
 			if checkIfTaken(square, o_locations, isXTurn):
 				x_locations.append(square)
 
-	elif not isXTurn:
+	else:
 		if event.button_index == BUTTON_LEFT and event.pressed: # Check if the Left Mouse Button was pressed
-			var square = getSquare(event.position)
-			if checkIfTaken(square, o_locations, isXTurn):
+			if checkIfTaken(x_locations, square, isXTurn):
 				o_locations.append(square)
 
 
@@ -57,8 +54,6 @@ func getSquare(pos):
 
 		elif pos.y >= 360 and pos.y <= 480:
 			return Vector2(305, 340)
-
-# :musical_note::notes::joy:(Yeah:thumbsup: , aye):joy: It's:point_right:the:salt:thirst:droplet::cold_sweat:, thirstiest:hot_face::sweat_drops:time:alarm_clock:, of:cup_with_straw:the:warning:year:calendar_spiral:. (Aye:rofl:, yuh:ok_hand_tone4:):eyes:I:eye:have:flushed:just:point_up_tone4:one:one:query:question:(:flushed:What's:question:that?:thinking:)Wanna:man_raising_hand_tone4:Sprite:cup_with_straw::sunglasses:Cranberry:cherries::sweat_drops:?(:smirk:Uh:eyes:huh:smirk:)The:weary:answer :drooling_face:is :tumbler_glass:clear..:yum::wink:It's:point_right:the:hot_face::flushed:thirst,:shushing_face::face_with_hand_over_mouth:thirstiest:sweat_drops::sweat_drops::100:time:alarm_clock:, of:cup_with_straw:the:cherries:year.:date:(:cup_with_straw:Sprite:cherries:,let's:cherries::cherries::100:go:man_running_tone4:):notes::musical_note:
 
 	elif pos.x >= 452 and pos.x <= 572: # 1, x
 
@@ -89,13 +84,18 @@ func getSquare(pos):
 func _input(event):
 
 	if event is InputEventMouseButton:
-		print("I got here")
 		var loc = getSquare(event.position)
-		if loc == Vector2(0, 0):
+		print(isXTurn)
+		if loc == Vector2(0, 0) or not event.pressed:
 			pass
 		else:
-			setSquare(event)
+			setSquare(event, loc)
+			if isXTurn:
+				isXTurn = false
+			else:
+				isXTurn = true
 			update()
+		print(isXTurn)
 
 
 func _draw():
@@ -110,10 +110,12 @@ func _draw():
 
 
 
-#func _draw(): # DEBUG
-#	var point = PoolVector2Array([Vector2(512, 0), Vector2(512, 600)])
-#	var color = PoolColorArray([Color(0, 0, 0)])
-#	draw_primitive(point, color, PoolVector2Array(), null, 50)
-
-#var xTexture = $spriteX.texture.get_data()
-#var oTexture = $spriteO.texture.get_data()
+# Wanna Sprite Cranberry emoji spam (Discord)
+# :musical_note::notes::joy:(Yeah:thumbsup: , aye):joy: It's:point_right:the:salt:thirst:droplet:
+# :cold_sweat:, thirstiest:hot_face::sweat_drops:time:alarm_clock:, of:cup_with_straw:the:warning:year:calendar_spiral:.
+# (Aye:rofl:, yuh:ok_hand_tone4:):eyes:I:eye:have:flushed:just:point_up_tone4:
+# one:one:query:question:(:flushed:What's:question:that?:thinking:)Wanna
+# :man_raising_hand_tone4:Sprite:cup_with_straw::sunglasses:Cranberry:cherries::sweat_drops:?(:smirk:Uh:eyes:huh
+# :smirk:)The:weary:answer :drooling_face:is :tumbler_glass:clear..:yum::wink:It's:point_right:the:hot_face:
+# :flushed:thirst,:shushing_face::face_with_hand_over_mouth:thirstiest:sweat_drops::sweat_drops::100:time:alarm_clock:, of:cup_with_straw:the:cherries:
+# year.:date:(:cup_with_straw:Sprite:cherries:,let's:cherries::cherries::100:go:man_running_tone4:):notes::musical_note:
--- README.md
+++ README.md
@@ -11,5 +11,5 @@ I have quite a few things to do before this is finished.
 - [x] Get letters to render
 - [x] Render the letters in the correct place
 - [x] Remove `Nil` exception when clicked outside of the grid
-- [ ] Switch turns every time the letter is placed
+- [x] Switch turns every time the letter is placed
 - [ ] Check the gamestate for if someone won
